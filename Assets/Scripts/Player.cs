db55767ebf484833c734dc43bf0de144cf54a6cb
phutaekwondo <lainguyenvinhphu@gmail.com>
2023-08-09 17:22:56 +0700

    move and look around

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] protected Camera m_camera;
+    [SerializeField] float m_mouseSentivity = 1f;
+    [SerializeField] float m_speed = 1f;
+    private Rigidbody m_rb;
+    PlayerState m_state;
+
+    private void Awake() {
+        m_state = new PlayerState(this);
+        m_rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        m_state.Update();
+    }
+
+    public class PlayerState
+    {
+        protected Player m_player;
+
+        public PlayerState(Player player)
+        {
+            m_player = player;
+        }
+        protected void CameraAngleUpdate()
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            m_player.m_camera.transform.Rotate(-mouseY * m_player.m_mouseSentivity, 0, 0);
+            m_player.transform.Rotate(0, mouseX * m_player.m_mouseSentivity, 0);
+        }
+        protected void HandleWalkInput()
+        {
+            float side = Input.GetAxis("Horizontal");
+            float straight = Input.GetAxis("Vertical");
+
+            Vector3 horizontalMove = m_player.transform.right * side + m_player.transform.forward * straight;
+            horizontalMove.Normalize();
+            horizontalMove *= m_player.m_speed * Time.deltaTime;
+
+            Debug.Log(horizontalMove);
+
+            Vector3 currentVelocity = m_player.m_rb.velocity;
+            currentVelocity.x = 0;
+            currentVelocity.z = 0;
+
+            Vector3 targetVelocity = currentVelocity + horizontalMove;
+            m_player.m_rb.velocity = targetVelocity;
+        }
+
+        public virtual void Update()
+        {
+            CameraAngleUpdate();
+            HandleWalkInput();
+        }
     }
 }
