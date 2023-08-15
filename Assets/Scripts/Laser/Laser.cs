ac11872ad36b554f0c55f78314294e0139babe7c
phutaekwondo <lainguyenvinhphu@gmail.com>
2023-08-15 16:47:05 +0700

    scratch laser interactable object logic

--- Assets/Scripts/Laser/Laser.cs
+++ Assets/Scripts/Laser/Laser.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,49 +5,61 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     float m_length = 10;
-    float m_maxLength = 10;
     LineRenderer m_lineRenderer;
     Vector3 m_startPosition;
-    Vector3 m_direction;
+    Vector3 m_startDirection;
+    Vector3 m_lastDirection;
+    List<Vector3> m_bouncePoints = new List<Vector3>();
 
     void Awake()
     {
         m_lineRenderer = GetComponent<LineRenderer>();
         m_startPosition = transform.position;
-        m_direction = transform.up;
+        m_startDirection = transform.up;
+        m_lastDirection = m_startDirection;
     }
 
     public void SetLaserStart(Vector3 startPosition, Vector3 direction)
     {
         this.m_startPosition = startPosition;
-        this.m_direction = direction;
-        UpdateLaserStart();
+        this.m_startDirection = direction;
+        m_lastDirection = m_startDirection;
     }
 
     private void FixedUpdate() 
     {
-        UpdateLaser();
+        UpdateLaserPath();
+        UpdateLineRenderer();
+
+        //clear the bounce points
+        m_bouncePoints.Clear();
     }
 
-    private void UpdateLaser()
+    private void UpdateLaserPath()
     {
         // Raycast
         RaycastHit hit;
-        if (Physics.Raycast(m_startPosition, m_direction, out hit, m_length))
+        if (Physics.Raycast(m_startPosition, m_startDirection, out hit, m_length))
         {
-            m_length = hit.distance;
-        }
-        else
-        {
-            m_length = m_maxLength;
+            GameObject hitObject = hit.collider.gameObject;
+            LaserInteractable laserInteractable = hitObject.GetComponent<LaserInteractable>();
+            if (laserInteractable != null)
+            {
+                m_bouncePoints.Add(hit.point);
+                m_lastDirection = laserInteractable.BounceDirection(m_startDirection);
+            }
         }
     }
 
-    private void UpdateLaserStart()
+    private void UpdateLineRenderer()
     {
-        m_lineRenderer.positionCount = 2;
-        m_lineRenderer.SetPosition(0, m_startPosition);
-        m_lineRenderer.SetPosition(1, m_startPosition + m_direction * m_length);
+        List<Vector3> positions = new List<Vector3>();
+        positions.Add(m_startPosition);
+        positions.AddRange(m_bouncePoints);
+        positions.Add(positions[positions.Count-1] + m_lastDirection * m_length);
+
+        m_lineRenderer.positionCount = positions.Count;
+        m_lineRenderer.SetPositions(positions.ToArray());
     }
 
 }
--- Assets/Scripts/Laser/LaserInteractable.cs
+++ Assets/Scripts/Laser/LaserInteractable.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserInteractable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    virtual public Vector3 BounceDirection(Vector3 incomingDirection)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return this.transform.forward;
     }
 }
